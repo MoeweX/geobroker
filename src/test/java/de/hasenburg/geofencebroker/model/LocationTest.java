432eb834397cdb71d59b8c540c63dcc2b70e5f4f
MoeweX <Jonathan@Hasenburg.de>
2018-08-27 15:47:01 +0200

    Add distance test

--- src/test/java/de/hasenburg/geofencebroker/model/LocationTest.java
+++ src/test/java/de/hasenburg/geofencebroker/model/LocationTest.java
@@ -32,4 +32,14 @@ public class LocationTest {
 		logger.info("FINISHED TEST");
 	}
 
+	@Test
+	public void testDistanceBerlinHamburg() {
+		logger.info("RUNNING testDistance TEST");
+		Location location = new Location(52.5200, 13.4050);
+		Location location2 = new Location(53.511, 9.9937);
+		logger.info(Location.distanceInMeters(location, location2));
+		assertEquals(253375, Location.distanceInMeters(location, location2), 0.5);
+		logger.info("FINISHED TEST");
+	}
+
 }
