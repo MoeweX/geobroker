f74c66aa143f9861630ecf2ae7b6a28a7f1248a7
MoeweX <Jonathan@Hasenburg.de>
2019-05-23 17:37:27 +0200

    [Kotlin] Migrate Raster

---
+++ GeoBroker-Server/src/main/kotlin/de/hasenburg/geobroker/server/storage/Raster.kt
@@ -0,0 +1,191 @@
+package de.hasenburg.geobroker.server.storage
+
+import de.hasenburg.geobroker.commons.exceptions.RuntimeStorageException
+import de.hasenburg.geobroker.commons.model.spatial.Geofence
+import de.hasenburg.geobroker.commons.model.spatial.Location
+import org.apache.commons.lang3.tuple.ImmutablePair
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+private val logger = LogManager.getLogger()
+
+/**
+ * Creates a new Raster. The index of each raster entry is the location of the entry's south west corner
+ *
+ * The raster always contains the whole world. Limitations to allowed subscriptions, e.g., the server should only
+ * accept subscriptions from Europe, have to be made on another level
+ *
+ * The granularity is used to calculate the size of each [RasterEntry] in degrees. The size equals 1 degree /
+ * granularity
+ *
+ * @param granularity - must be >= 1
+ * @throws RuntimeStorageException if granularity < 1
+ */
+class Raster(val granularity: Int) {
+
+    private val rasterEntries = ConcurrentHashMap<Location, RasterEntry>()
+    val degreeStep: Double // = 1 / granularity
+
+    val numberOfExistingRasterEntries: Int
+        get() = rasterEntries.size
+
+    init {
+        if (granularity < 1) {
+            throw RuntimeStorageException("Granularity must be >= 1, is $granularity")
+        }
+        this.degreeStep = 1.0 / granularity
+        logger.debug("Raster created, granularity = {}, degree step = {}", granularity, degreeStep)
+    }
+
+    /*****************************************************************
+     * Subscribe/Unsubscribe Operations
+     ****************************************************************/
+
+    /**
+     * Adds a subscriptionId to the fitting [RasterEntry]s
+     *
+     * @param geofence - the geofence used to calculate the fitting [RasterEntry]
+     * @param subscriptionId - the subscriptionId to be added
+     */
+    fun putSubscriptionIdIntoRasterEntries(geofence: Geofence, subscriptionId: ImmutablePair<String, Int>) {
+        // get all RasterEntries to which the id should be added
+        val viableRasterEntries = calculateIndexLocations(geofence)
+
+        // add the subscriptionId to the RasterEntries
+        for (viableRasterEntry in viableRasterEntries) {
+            viableRasterEntry.putSubscriptionId(subscriptionId)
+        }
+    }
+
+    /**
+     * Removes a subscriptionId from the fitting [RasterEntry]s. Probably used for unsubscribe operations.
+     *
+     * @param geofence - the geofence used to calculate the fitting [RasterEntry]
+     * @param subscriptionId - the subscription id to be removed
+     */
+    fun removeSubscriptionIdFromRasterEntries(geofence: Geofence, subscriptionId: ImmutablePair<String, Int>) {
+        // get all RasterEntries from which the id should be removed
+        val viableRasterEntries = calculateIndexLocations(geofence)
+
+        // remove the subscriptionId from the RasterEntries
+        for (viableRasterEntry in viableRasterEntries) {
+            viableRasterEntry.removeSubscriptionId(subscriptionId)
+        }
+    }
+
+    /**
+     * Removes a subscriptionId from a single [RasterEntry]. Probably used when a subscription is renewed with a
+     * new geofence and outdated subscription ids need to be removed.
+     * If no subscription existed, does nothing.
+     *
+     * @param index - index of [RasterEntry] from which the subscription id should be removed
+     * @param subscriptionId - the subscription id to be removed
+     */
+    fun removeSubscriptionIdFromRasterEntry(index: Location, subscriptionId: ImmutablePair<String, Int>) {
+        rasterEntries[index]?.removeSubscriptionId(subscriptionId)
+    }
+
+    /*****************************************************************
+     * Process Published Message Operations
+     ****************************************************************/
+
+    /**
+     * Returns all subscriptionIds that are in the fitting [RasterEntry]. In case no subscription ids exist, the
+     * returned Map is empty.
+     *
+     * @param location - the location that determines which [RasterEntry] fits
+     * @return a Map containing all fitting subscriptionIds; client -> set of subscription ids
+     */
+    fun getSubscriptionIdsForPublisherLocation(location: Location): Map<String, Set<ImmutablePair<String, Int>>> {
+        val index = calculateIndexLocation(location)
+        return rasterEntries[index]?.allSubscriptionIds ?: emptyMap()
+    }
+
+    /*****************************************************************
+     * Methods to calculate Indices
+     ****************************************************************/
+
+    /**
+     * Calculates at what index the corresponding [RasterEntry] is stored
+     *
+     * @param location - the location
+     * @return - the index
+     */
+    private fun calculateIndexLocation(location: Location): Location {
+        val latIndex = Math.floor(location.lat * granularity) / granularity
+        val lonIndex = Math.floor(location.lon * granularity) / granularity
+
+        return Location(latIndex, lonIndex)
+    }
+
+    /**
+     * Calculates with which [RasterEntry] the given geofence intersects with.
+     *
+     * @param geofence - the geofence
+     * @return - a list of [RasterEntry]s
+     */
+    private fun calculateIndexLocations(geofence: Geofence): List<RasterEntry> {
+        // get north east and south west indices
+        val northEastIndex = calculateIndexLocation(geofence.boundingBoxNorthEast)
+        val southWestIndex = calculateIndexLocation(geofence.boundingBoxSouthWest)
+
+        // get raster entries that have to be checked for intersection
+        val rasterEntriesToCheckForIntersection = ArrayList<RasterEntry>()
+        // due to double precision, we need to check whether > -0.000000001
+        var lat = southWestIndex.lat
+        while (northEastIndex.lat - lat > -0.000000001) {
+            var lon = southWestIndex.lon
+            while (northEastIndex.lon - lon > -0.000000001) {
+                lat = Math.round(lat * granularity) / granularity.toDouble()
+                lon = Math.round(lon * granularity) / granularity.toDouble()
+                val index = Location(lat, lon)
+                val re = rasterEntries.getOrPut(index) { RasterEntry(index, degreeStep) }
+                rasterEntriesToCheckForIntersection.add(re)
+                lon += degreeStep
+            }
+            lat += degreeStep
+        }
+
+        // if geofence is a rectangle, we can collect the indices
+        if (geofence.isRectangle) {
+            return rasterEntriesToCheckForIntersection
+        }
+
+        // remove raster entries whose box is disjoint with the actual geofence
+        rasterEntriesToCheckForIntersection.removeIf { re -> re.rasterEntryBox.disjoint(geofence) }
+
+        // return
+        return rasterEntriesToCheckForIntersection
+    }
+
+    /*****************************************************************
+     * No Geo-Context (used to calculate Overhead)
+     *
+     * In order to use the storage without geo-context information,
+     * the two private methods above need to be commented out.
+     *
+     * Note: the below is still Java code
+     ****************************************************************/
+
+//    	private final Location index = new Location(0.0, 0.0);
+//
+//    	private Location calculateIndexLocation(Location location) {
+//    		return index;
+//    	}
+//
+//    	/**
+//    	 * Calculates with which {@link RasterEntry} the given geofence intersects with.
+//    	 *
+//    	 * @param geofence - the geofence
+//    	 * @return - a list of {@link RasterEntry}s
+//    	 */
+//    	private List<RasterEntry> calculateIndexLocations(Geofence geofence) {
+//    		List<RasterEntry> list = new ArrayList<>();
+//    		list.add(rasterEntries.computeIfAbsent(index, k -> new RasterEntry(index, degreeStep))); // only one entry
+//    		return list;
+//    	}
+
+}
---
+++ GeoBroker-Server/src/test/kotlin/de/hasenburg/geobroker/server/storage/RasterTest.kt
@@ -0,0 +1,256 @@
+package de.hasenburg.geobroker.server.storage
+
+import de.hasenburg.geobroker.commons.model.spatial.Geofence
+import de.hasenburg.geobroker.commons.model.spatial.Location
+import de.hasenburg.geobroker.server.storage.Raster
+import de.hasenburg.geobroker.server.storage.RasterEntry
+import org.apache.commons.lang3.tuple.ImmutablePair
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.locationtech.spatial4j.exception.InvalidShapeException
+
+import java.lang.reflect.InvocationTargetException
+import java.lang.reflect.Method
+import java.util.Arrays
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Assert.fail
+
+private val logger = LogManager.getLogger()
+
+@Suppress("PrivatePropertyName")
+class RasterTest {
+
+    private var privateMethod_calculateIndexLocation: Method? = null
+    private var privateMethod_calculateIndexLocations: Method? = null
+    private var raster: Raster? = null
+
+    @Before
+    @Throws(NoSuchMethodException::class)
+    fun setUpTest() {
+        privateMethod_calculateIndexLocation = Raster::class.java.getDeclaredMethod("calculateIndexLocation",
+                Location::class.java)
+        privateMethod_calculateIndexLocations = Raster::class.java.getDeclaredMethod("calculateIndexLocations",
+                Geofence::class.java)
+
+        privateMethod_calculateIndexLocation!!.isAccessible = true
+        privateMethod_calculateIndexLocations!!.isAccessible = true
+    }
+
+    @After
+    fun tearDownTest() {
+        privateMethod_calculateIndexLocation = null
+        privateMethod_calculateIndexLocations = null
+        raster = null
+    }
+
+    private fun invokeCalculateIndexLocation(location: Location): Location? {
+        try {
+            return privateMethod_calculateIndexLocation!!.invoke(raster, location) as Location
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+            fail("Could not invoke private method")
+        } catch (e: InvocationTargetException) {
+            e.printStackTrace()
+            fail("Could not invoke private method")
+        }
+
+        // Stupid, I never get here, why do I need to return something?
+        return null
+    }
+
+    private fun invokeCalculateIndexLocations(geofence: Geofence): List<RasterEntry>? {
+        try {
+            return privateMethod_calculateIndexLocations!!.invoke(raster, geofence) as List<RasterEntry>
+        } catch (e: IllegalAccessException) {
+            e.printStackTrace()
+            fail("Could not invoke private method")
+        } catch (e: InvocationTargetException) {
+            e.printStackTrace()
+            fail("Could not invoke private method")
+        }
+
+        // Stupid, I never get here, why do I need to return something?
+        return null
+    }
+
+    @Test(expected = InvalidShapeException::class)
+    fun testCalculateIndexGranularity1() {
+        raster = Raster(1)
+        var calculatedIndex = invokeCalculateIndexLocation(Location(10.0, -10.0))
+
+        // even
+        assertEquals(Location(10.0, -10.0), calculatedIndex)
+
+        // many fractions
+        calculatedIndex = invokeCalculateIndexLocation(Location(10.198, -11.198))
+        assertEquals(Location(10.0, -12.0), calculatedIndex)
+
+        // exact boundary
+        calculatedIndex = invokeCalculateIndexLocation(Location(90.0, -180.0))
+        assertEquals(Location(90.0, -180.0), calculatedIndex)
+
+        // out of bounds, expect throw
+        invokeCalculateIndexLocation(Location(91.0, -181.0))
+    }
+
+    @Test(expected = InvalidShapeException::class)
+    fun testCalculateIndexGranularity10() {
+
+        raster = Raster(10)
+        var calculatedIndex = invokeCalculateIndexLocation(Location(10.0, -10.0))
+
+        // even
+        assertEquals(Location(10.0, -10.0), calculatedIndex)
+
+        // many fractions
+        calculatedIndex = invokeCalculateIndexLocation(Location(10.198, -11.198))
+        assertEquals(Location(10.1, -11.2), calculatedIndex)
+
+        // exact boundary
+        calculatedIndex = invokeCalculateIndexLocation(Location(90.0, -180.0))
+        assertEquals(Location(90.0, -180.0), calculatedIndex)
+
+        // out of bounds, expect throw
+        invokeCalculateIndexLocation(Location(91.0, -181.0))
+    }
+
+    @Test(expected = InvalidShapeException::class)
+    fun testCalculateIndexGranularity100() {
+
+        raster = Raster(100)
+        var calculatedIndex = invokeCalculateIndexLocation(Location(10.0, -10.0))
+
+        // even
+        assertEquals(Location(10.0, -10.0), calculatedIndex)
+
+        // many fractions
+        calculatedIndex = invokeCalculateIndexLocation(Location(10.198, -11.198))
+        assertEquals(Location(10.19, -11.2), calculatedIndex)
+
+        // exact boundary
+        calculatedIndex = invokeCalculateIndexLocation(Location(90.0, -180.0))
+        assertEquals(Location(90.0, -180.0), calculatedIndex)
+
+        // out of bounds, expect throw
+        invokeCalculateIndexLocation(Location(91.0, -181.0))
+    }
+
+    @Test
+    fun testCalculateIndexLocationsForGeofenceRectangle() {
+        raster = Raster(1)
+        val fence = Geofence.polygon(Arrays.asList(Location(-0.5, -0.5),
+                Location(-0.5, 1.5),
+                Location(1.5, 1.5),
+                Location(1.5, -0.5)))
+
+        val result = invokeCalculateIndexLocations(fence)
+        assertEquals(9, result!!.size.toLong())
+        assertTrue(containsLocation(result, Location(-1.0, -1.0)))
+        assertTrue(containsLocation(result, Location(-1.0, 0.0)))
+        assertTrue(containsLocation(result, Location(-1.0, 1.0)))
+        assertTrue(containsLocation(result, Location(0.0, -1.0)))
+        assertTrue(containsLocation(result, Location(0.0, 0.0)))
+        assertTrue(containsLocation(result, Location(0.0, 1.0)))
+        assertTrue(containsLocation(result, Location(1.0, -1.0)))
+        assertTrue(containsLocation(result, Location(1.0, 0.0)))
+        assertTrue(containsLocation(result, Location(1.0, 1.0)))
+    }
+
+    @Test
+    fun testCalculateIndexLocationsForGeofenceTriangle() {
+        raster = Raster(1)
+        val fence = Geofence.polygon(Arrays.asList(Location(-0.5, -1.5), Location(-0.5, 0.7), Location(1.7, -1.5)))
+
+        val result = invokeCalculateIndexLocations(fence)
+        assertEquals(8, result!!.size.toLong())
+        assertTrue(containsLocation(result, Location(-1.0, -2.0)))
+        assertTrue(containsLocation(result, Location(-1.0, -1.0)))
+        assertTrue(containsLocation(result, Location(-1.0, 0.0)))
+        assertTrue(containsLocation(result, Location(0.0, -2.0)))
+        assertTrue(containsLocation(result, Location(0.0, -1.0)))
+        assertTrue(containsLocation(result, Location(0.0, 0.0)))
+        assertTrue(containsLocation(result, Location(1.0, -2.0)))
+        assertTrue(containsLocation(result, Location(1.0, -1.0)))
+    }
+
+    @Test
+    fun testCalculateIndexLocationsForGeofenceCircle() {
+        raster = Raster(1)
+        val fence = Geofence.circle(Location(0.5, 0.0), 1.1)
+
+        val result = invokeCalculateIndexLocations(fence)
+        assertEquals(8, result!!.size.toLong())
+        assertTrue(containsLocation(result, Location(-1.0, -1.0)))
+        assertTrue(containsLocation(result, Location(-1.0, 0.0)))
+        assertTrue(containsLocation(result, Location(0.0, -2.0)))
+        assertTrue(containsLocation(result, Location(0.0, -1.0)))
+        assertTrue(containsLocation(result, Location(0.0, 0.0)))
+        assertTrue(containsLocation(result, Location(0.0, 1.0)))
+        assertTrue(containsLocation(result, Location(1.0, -1.0)))
+        assertTrue(containsLocation(result, Location(1.0, 0.0)))
+    }
+
+    @Test
+    fun testCalculateIndexLocationsForGeofenceCircle2() {
+        raster = Raster(5)
+        val l = Location(39.984702, 116.318417)
+        val fence = Geofence.circle(l, 0.1)
+
+        val result = invokeCalculateIndexLocations(fence)
+        assertTrue(containsLocation(result!!, Location(39.8, 116.2)))
+        assertTrue(containsLocation(result, Location(39.8, 116.4)))
+        assertTrue(containsLocation(result, Location(40.0, 116.2)))
+        assertTrue(containsLocation(result, Location(40.0, 116.4)))
+        assertEquals(4, result.size.toLong())
+    }
+
+    @Test
+    fun testCalculateIndexLocationsForGeofenceCircle3() {
+        raster = Raster(10)
+        val l = Location(39.984702, 116.318417)
+        val fence = Geofence.circle(l, 0.1)
+
+        val result = invokeCalculateIndexLocations(fence)
+        assertTrue(containsLocation(result!!, Location(39.8, 116.2)))
+        assertTrue(containsLocation(result, Location(39.8, 116.3)))
+        assertTrue(containsLocation(result, Location(39.9, 116.2)))
+        assertTrue(containsLocation(result, Location(39.9, 116.3)))
+        assertTrue(containsLocation(result, Location(39.9, 116.4)))
+        assertTrue(containsLocation(result, Location(40.0, 116.2)))
+        assertTrue(containsLocation(result, Location(40.0, 116.3)))
+        assertTrue(containsLocation(result, Location(40.0, 116.4)))
+        assertEquals(8, result.size.toLong())
+    }
+
+    @Test
+    fun testPutAndThenGet() {
+        raster = Raster(25)
+        val l = Location(40.007499, 116.320013)
+        val fence = Geofence.circle(l, 0.01)
+        val sid = ImmutablePair("test", 1)
+
+        val result = invokeCalculateIndexLocations(fence)
+        val index = invokeCalculateIndexLocation(l)
+        assertTrue(containsLocation(result!!, index))
+
+        raster!!.putSubscriptionIdIntoRasterEntries(fence, sid)
+        val ids = raster!!.getSubscriptionIdsForPublisherLocation(l)
+        logger.info(ids)
+    }
+
+    private fun containsLocation(result: List<RasterEntry>, l: Location?): Boolean {
+        for (rasterEntry in result) {
+            if (rasterEntry.index == l) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+}
